e20851f0d788621cafd67f9f7b6ad2d41b4964f1
samipiit <sami@piit.us>
2022-04-03 00:12:27 -0400

    Updated instructions

--- src/company_app_design/CompanyEmployee.java
+++ src/company_app_design/CompanyEmployee.java
@@ -2,14 +2,14 @@ package company_app_design;
 
 public class CompanyEmployee {
 
-    /**
-     *  CompanyEmployee class has a main method where you will be able to create Object from
-     *         EmployeeInfo class to use fields and attributes
-     *      Demonstrate as many methods as possible to use with proper business work flow.
-     *         Try to think like a Software Architect/Developer.
-     *         This application should provide basic employee information services
+    /** INSTRUCTIONS
+     *  This class has a main method where you will be able to create instances of the EmployeeInfo class in order
+     *  to use attributes and methods.
      *
-     *      Use any databases[MongoDB, Oracle, MySql] to store data_structures.data and retrieve data_structures.data.
+     *  Demonstrate as many methods as possible here. Feel free to show off what you've learned so far.
+     *
+     *  The goal of this application is to provide basic employee information services
+     *  Try to think like a Software Developer
      */
     public static void main(String[] args) {
 
