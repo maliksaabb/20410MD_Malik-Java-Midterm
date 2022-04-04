0f7e060172d6ba7b995f8b6f84d95de902ced1f1
maliksaabb <usmandotmalik@gmail.com>
2022-04-04 18:56:19 -0400

    Adding factorial

--- src/math_problems/Factorial.java
+++ src/math_problems/Factorial.java
@@ -9,8 +9,27 @@ public class Factorial {
      * HINT: Factorial of 5! = 5 x 4 X 3 X 2 X 1 = 120
      */
 
-    public static void main(String[] args) {
+    static int Recursion_factorial(int num) {
+        if (num == 0)
+            return 1;
 
+        return num * Recursion_factorial(num - 1);
+    }
+
+    static int Iteration_factorial(int n)
+    {
+        int fac = 1, i;
+        for (i = fac; i <= n; i++)
+            fac = fac * i;
+        return fac;
+    }
+
+    public static void main(String[] args) {
+        {
+            int num = 4;
+            System.out.println("Rec Factorial of " + num + " is " + Recursion_factorial(4));
+            System.out.println("Ite Factorial of " + num + " is " + Iteration_factorial(4));
+        }
 
     }
 
