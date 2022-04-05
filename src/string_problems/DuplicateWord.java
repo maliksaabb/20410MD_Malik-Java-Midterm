ac8b0873bae2cf2c8e5aea0dbb94eb30a96bdbde
maliksaabb <usmandotmalik@gmail.com>
2022-04-04 22:09:18 -0400

    Added DuplicateWord

--- src/string_problems/DuplicateWord.java
+++ src/string_problems/DuplicateWord.java
@@ -11,7 +11,28 @@ public class DuplicateWord {
 
         String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
 
-        // Implement here
+        int count;
+
+        //Converts the string into lowercase
+        st = st.toLowerCase();
+
+        String words[] = st.split(" ");
+
+        System.out.println("Duplicate words in a given string : ");
+        for(int i = 0; i < words.length; i++) {
+            count = 1;
+            for(int j = i+1; j < words.length; j++) {
+                if(words[i].equals(words[j])) {
+                    count++;
+                    //Set words[j] to 0 to avoid printing visited word
+                    words[j] = "0";
+                }
+            }
+
+            //Displays the duplicate word if count is greater than 1
+            if(count > 1 && words[i] != "0")
+                System.out.println(words[i]);
+        }
 
     }
 
