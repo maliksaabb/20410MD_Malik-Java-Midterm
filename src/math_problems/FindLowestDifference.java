a6d879279f7312491994078d5aa2ee6b86983ad5
maliksaabb <usmandotmalik@gmail.com>
2022-04-06 13:35:16 -0400

    Commiting LowestDiff & MissingNumber probs

--- src/math_problems/FindLowestDifference.java
+++ src/math_problems/FindLowestDifference.java
@@ -14,8 +14,39 @@ public class FindLowestDifference {
         int[] array1 = {30, 12, 5, 9, 2, 20, 33, 1, -15};
         int[] array2 = {18, 25, 41, 47, 17, 36, 14, 19, -15};
 
+        int[] array3 = new int[array1.length + array2.length];
+        boolean n = true;
+        while (n) {
+            int min_array1 = array1[0];
+            int index1 = 0;
+            for (int i = 0; i < array1.length; i++) {
+                if (min_array1 > array1[i]) {
+                    min_array1 = array1[i];
+                    index1 = i;
+                }
+            }
+
+            int min_array2 = array2[0];
+            int index2 = 0;
+            for (int i = 0; i < array2.length; i++) {
+                if (min_array2 > array2[i]) {
+                    min_array2 = array2[i];
+                    index2 = i;
+                }
+
+            }
+            if (min_array1 == min_array2) {
+                array1[index1] = Integer.MAX_VALUE;
+                array2[index2] = Integer.MAX_VALUE;
+            } else if (min_array1 < min_array2) {
+                System.out.println(min_array1);
+                n = false;
+            } else {
+                System.out.println(min_array2);
+                n = false;
+            }
+
+        }
     }
-
-
 }
 
--- src/math_problems/FindMissingNumber.java
+++ src/math_problems/FindMissingNumber.java
@@ -1,13 +1,21 @@
 package math_problems;
 
-public class FindMissingNumber {
+import java.util.*;
+import java.util.Arrays;
+class  FindMissingNumber {
 
-    /** INSTRUCTIONS
-     * Write a method to find the missing number from the array.
-     */
+    public static int findMissingNumber(int[] array)
+    {
+        int n=array.length;
+        int sum=((n+1)*(n+2))/2;
+        for(int i=0;i<n;i++)
 
-    public static void main(String[] args) {
-        int[] array = new int[] {10, 2, 1, 4, 5, 3, 7, 8, 6};
-
+            sum-=array[i];
+        return sum;
+    }
+    public static void main(String[] args)
+    {
+        int[] a = {10, 2, 1, 4, 5, 3, 7, 8, 6};
+        System.out.println(findMissingNumber(a));
     }
 }
