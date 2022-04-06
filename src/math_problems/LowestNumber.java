c6155a9784e107d8853a5b315abbe6b11fd3ed76
maliksaabb <usmandotmalik@gmail.com>
2022-04-06 13:55:36 -0400

    Pushing LowestNumber prob

--- src/math_problems/LowestNumber.java
+++ src/math_problems/LowestNumber.java
@@ -11,7 +11,13 @@ public class LowestNumber {
         int[] array = new int[] {211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123,
                 67, 5, 679, 54, 32, 65};
 
-
+        int lowNum= array[0];
+        //Looping through the array
+        for (int i = 0; i < array.length; i++) {
+            //Compare elements of array with lowNum
+            if(array[i] <lowNum)
+                lowNum = array[i];
+        }
+        System.out.println("The lowest number in this array is " + lowNum);
     }
-
 }
